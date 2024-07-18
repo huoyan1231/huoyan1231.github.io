a639481216c3a76f8928792fe989a606f00a953e
huoyan1231 <53113734+huoyan1231@users.noreply.github.com>
2024-07-18 14:46:00 +0800

    1

--- sekai-stickers/src/views/HomeView.jsx
+++
@@ -1,8 +0,0 @@
-import { Text } from '@radix-ui/themes'
-export default function HomeView() {
-  return (
-    <div className="font-sans min-h-[100svh] flex flex-col items-center">
-        <Text size="8">Sekai Stickers</Text>
-    </div>
-  )
-}
--- sekai-stickers/src/views/NotFoundView.jsx
+++
@@ -1,8 +0,0 @@
-export default function NotFoundView () {
-    return (
-        <div className="font-sans h-[100svh] flex flex-col justify-center items-center">
-            <h1 className="text-2xl font-bold">Oops!</h1>
-            <p>Source not found :(</p>
-        </div>
-    )
-}
